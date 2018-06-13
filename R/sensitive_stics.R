5683e5348dba2a2e810837e0f1a0089eef5c8abd
VEZY Rémi <vezy.remi@gmail.com>
2018-06-13 12:03:27 +0200

    senitive_stics: Add sobol + change input format

--- R/sensitive_stics.R
+++ R/sensitive_stics.R
@@ -9,14 +9,30 @@
 #' @param obs_name   A vector of observation file name(s). It must have the form
 #'                   \code{c(Dominant,Dominated)} for mixed crops.
 #'                   See \code{\link{read_obs}} \code{filename} parameter for more details.
-#' @param Parameters A vector of input parameter names
+#' @param Parameters A list of list of min and max values for each parameters, named after
+#'                   them (see details and example)
 #' @param Vars       Output variables on which the sensitivity is performed
 #' @param method     The sensitivity method to use, see \pkg{sensitivity} package
+#' @param n          Sample size for simulation (must an even number for `sobol` alike
+#'                   methods)
+#' @param q          A vector of quantile functions names corresponding to wanted factors
+#'                   distributions (see \code{\link[sensitivity]{fast99}} and details).
+#'                   \code{\link[sensitivity]{fast99}} and details).
 #' @param Plant      On which plant (\emph{i.e.} Principal or associated) the parameters
 #'                   have to be set (only for mixed simulations, using plant parameters)
 #'                   Set to \code{NULL} if using STICS in sole crop
 #' @param ...        Further parameters to give to the sensitivity function used
 #'                   (see \pkg{sensitivity} package)
+#'
+#' @details The function uses the \pkg{sensitivity} package functions under the hood to
+#'  create the DOE (design of experiment) and then to compute the sensitivity index. For
+#'  `sobol` alike methods, the DOE is first computed using the
+#'  \code{\link[sensitivity]{fast99}} function using the `q` and `Parameters` (for
+#'  `q.arg`) parameters. The DOE is then splitted in two to fill the `X1`` and `X2`
+#'  parameters.
+#'  The `Parameters` should take the form of a list of arguments to pass to the `q`
+#'  function for each parameter, named after the parameter of interest (see example).
+#'
 #'
 #' @return A list of two :
 #' \itemize{
@@ -28,43 +44,66 @@
 #'
 #' @importFrom sensitivity fast99 sobol tell
 #' @importFrom dplyr group_by summarise
+#' @importFrom caret createDataPartition
 #'
 #' @seealso \code{\link[stats]{Distributions}} if you use \code{\link[sensitivity]{fast99}}.
 #'
 #' @examples
 #'\dontrun{
 #' library(sticRs)
-#' # Example using the fast99 method:
+#' # Example using the fast99 method for a sensitivity analysis on the
+#' # "interrang" and "P_densitesem" parameters:
+#'
 #' sensitive_stics(dir.orig = "0-DATA/dummy/SC_Wheat",
 #'                 dir.targ = "2-Simulations/Sensitivity2",
 #'                 stics = "0-DATA/stics_executable/EquDens_trg/stics.exe",
-#'                 obs_name = "Wheat_N0.obs",Parameters = "interrang",
+#'                 obs_name = "Wheat_N0.obs",
+#'                 Parameters= list(interrang= list(min=0.05, max=0.25),
+#'                                  P_densitesem= list(min=140, max=280)),
 #'                 Vars = c("raint", "lai(n)", "masec(n)"),
 #'                 method= "fast99", n= 10,
 #'                 q= "qunif",q.arg = list(list(min=0.05, max=0.25),
+#'                 list(min=140, max=280)))
+#'
+#' # Example using the sobol method for a sensitivity analysis on the
+#' # "interrang" and "P_densitesem" parameters with different quantile functions:
+#'
+#' sensitive_stics(dir.orig = "0-DATA/dummy/SC_Wheat",
+#'                 dir.targ = "2-Simulations/Sensitivity2",
+#'                 stics = "0-DATA/stics_executable/EquDens_trg/stics.exe",
+#'                 obs_name = "Wheat_N0.obs",
+#'                 Parameters= list(interrang= list(min=0.05, max=0.25),
+#'                                  P_densitesem= list(mean=210, sd=30)),
+#'                 Vars = c("raint", "lai(n)", "masec(n)"),
+#'                 method= "fast99", n= 10,
+#'                 q= c("qunif","qnorm"),q.arg = list(list(min=0.05, max=0.25),
 #'                 list(min=140, max=280)))
 #'}
 #'
 #' @export
 #'
 sensitive_stics= function(dir.orig, dir.targ=getwd(),stics,obs_name,Parameters,
-                          Vars,method=c("fast99","sobol"),Plant=1,...){
+                          Vars,method=c("fast99","sobol"),n=10*length(Vars),
+                          q="qunif",Plant=1,...){
   .=Date=Dominance=S_Max=S_Mean=S_Min=Sim=meas=plant=sd_meas=NULL
-  method= match.arg(method,c("fast99"))
+  method= match.arg(method,c("fast99","sobol"))
 
-  if(method=="fast99"){
-    Design_experiment= sensitivity::fast99(model= NULL,
-                                           factors = Parameters, ...)
-  }else if(method=="sobol"){
-    Design_experiment= sensitivity::sobol(model = NULL, ...)
+  Design_experiment= sensitivity::fast99(model= NULL,factors = names(Parameters),
+                                         n=n,q=q,q.arg=Parameters)
+  DOE= Design_experiment$X
+  if(any(is.na(DOE))){
+    stop("Sample size too little for evaluation, please increase the n parameter")
+  }
+
+  if(method=="sobol"){
+    if(n%%2==1){stop("n must be an even number to partition equally the input for sobol")}
+    Sample_index= sample(x= 1:nrow(DOE),size= floor(nrow(DOE)/2),replace= F)
+    X1= DOE[Sample_index,,drop=F]
+    X2= DOE[-Sample_index,,drop=F]
+    Design_experiment= sensitivity::sobol(model= NULL,X1= X1,X2= X2, ...)
   }
 
   Vars_R= gsub("\\(","_",Vars)%>%gsub("\\)","",.)
-
-  DOE= Design_experiment$X
-  if(method=="fast99"){
-    if(any(is.na(DOE))){"Please increase the n parameter of the fast99 function"}
-  }
 
   NbCores= parallel::detectCores()-1
   cl= parallel::makeCluster(min(NbCores,nrow(DOE)))
@@ -87,7 +126,7 @@ sensitive_stics= function(dir.orig, dir.targ=getwd(),stics,obs_name,Parameters,
                           set_out_var(filepath= file.path(USM_path,"var.mod"),
                                       vars=Vars, app=F)
 
-                          lapply(Parameters, function(pa){
+                          lapply(names(Parameters), function(pa){
                             set_param(dirpath = USM_path, param = pa,
                                       value = DOE[x,pa],plant = Plant)
                           })
@@ -138,7 +177,7 @@ sensitive_stics= function(dir.orig, dir.targ=getwd(),stics,obs_name,Parameters,
                              title=paste("Sensitivity analysis for the",
                                          Vars_R[x],"variable"),
                              subtitle= paste("Parameter(s) tested:",
-                                             paste(Parameters,
+                                             paste(names(Parameters),
                                                    collapse = ", ")))
              if(is_meas){
                gg_sens=
