a91b5f2d7578ad6184508a4849264bf9727dec0d
VEZY Rémi <vezy.remi@gmail.com>
2018-08-01 17:32:26 +0200

    Moved example data into test folder

--- tests/testthat/test-read_met.R
+++ tests/testthat/test-read_met.R
@@ -3,5 +3,5 @@ context("Read meteo file (read_met)")
 library(sticRs)
 
 test_that("read_met returns a data.frame", {
-  expect_true(is.data.frame(read_met(dirpath = "C:/Users/vezyr/OneDrive/Postdoc/sticRs/example_data")))
+  expect_true(is.data.frame(read_met(dirpath = "example_data")))
 })
