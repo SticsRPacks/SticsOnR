4282f18e02a6e2132114e714a9e2f2ac84564a27
VEZY Rémi <vezy.remi@gmail.com>
2018-09-18 17:37:44 +0200

    Add group to plot_output + add_group helper function
    The group helps y-facetting the plots

--- R/plot_output.R
+++ R/plot_output.R
@@ -10,6 +10,9 @@
 #'                 \code{c(Dominant,Dominated)} for mixed crops. See details.
 #' @param Title    A title for the plot (optional)
 #' @param plot_it  Boolean. Do the plot as to be pinted ?
+#' @param group    Boolean (Optional). Should the \code{group} column be used for
+#'                 column faceting. If \code{TRUE}, make sure that the group column
+#'                 exists in the input
 #'
 #' @details if \code{Vars} is NULL (the default), the function plots all variables
 #' from the simulation. The output variables from simulations can be set using
@@ -46,7 +49,7 @@
 #'}
 #' @export
 #'
-plot_output= function(..., Vars=NULL,obs_name=NULL,Title=NULL,plot_it=T){
+plot_output= function(..., Vars=NULL,obs_name=NULL,Title=NULL,plot_it=T,group=F){
   Date= Dominance= value= Equ= variable= Version= .= value_min= value_max= NULL
   dot_args= list(...)
   if(any((sapply(dot_args,length)>1)&!(sapply(dot_args,is.data.frame)))){
@@ -76,6 +79,12 @@ plot_output= function(..., Vars=NULL,obs_name=NULL,Title=NULL,plot_it=T){
     Vars= gsub("\\(","_",Vars)%>%gsub("\\)","",.)
   }
 
+  if(group){
+    id.vars = c("Date","Dominance","Version","group")
+  }else{
+    id.vars = c("Date","Dominance","Version")
+  }
+
   x_sim_= x_meas_= .= NULL
   for(i in seq_along(dot_args)){
     x= dot_args[[i]]
@@ -83,22 +92,23 @@ plot_output= function(..., Vars=NULL,obs_name=NULL,Title=NULL,plot_it=T){
       x[,-grep("_meas|ian|mo|jo|jul",colnames(x))]
     x_sim$Version= V_names[i]
     colnames(x_sim)= gsub("_sim","",colnames(x_sim))
-    Vars= paste("Date|Dominance|Version",paste(Vars,collapse = "|"),sep="|")
+    Vars= paste("Date|Dominance|Version|group",
+                paste(Vars,collapse = "|"),sep="|")
     x_sim_=
       x_sim[,grep(Vars,colnames(x_sim))]%>%
-      reshape2::melt(id.vars = c("Date","Dominance","Version"))%>%
+      reshape2::melt(id.vars = id.vars)%>%
       rbind(x_sim_,.)
 
     x_meas=
       x[,-grep("_sim|ian|mo|jo|jul",colnames(x))]
     # If x_meas has more than Dominance, Date and Plant columns:
     if(ncol(x_meas)>3){
-      x_meas[,-grep("_meas|Date|Dominance|Plant",colnames(x_meas))]= NA
+      x_meas[,-grep("_meas|Date|Dominance|Plant|group",colnames(x_meas))]= NA
       x_meas$Version= V_names[i]
       colnames(x_meas)= gsub("_meas","",colnames(x_meas))
       x_meas_=
         x_meas[,grep(Vars,colnames(x_meas))]%>%
-        reshape2::melt(id.vars = c("Date","Dominance","Version"))%>%
+        reshape2::melt(id.vars = id.vars)%>%
         rbind(x_meas_,.)
     }
 
@@ -106,10 +116,17 @@ plot_output= function(..., Vars=NULL,obs_name=NULL,Title=NULL,plot_it=T){
 
   levels(x_sim_$variable)= gsub("_n","",levels(x_sim_$variable))
 
+
+  if(group){
+    ggstics= ggplot(x_sim_) +
+      facet_grid(vars(variable),vars(group), scales='free')
+  }else{
+    ggstics= ggplot(x_sim_) +
+      facet_grid(vars(variable), scales='free')
+  }
   ggstics=
-    ggplot(x_sim_,aes(x=Date))+
-    facet_grid(variable~., scales='free_y') +
-    geom_line(aes(y= value, colour= Dominance,linetype= Version))+
+    ggstics +
+    geom_line(aes(x=Date, y= value, colour= Dominance,linetype= Version))+
     labs(linetype='Simulation',colour='Plant dominance')+
     ggtitle(Title)
 
@@ -134,7 +151,7 @@ plot_output= function(..., Vars=NULL,obs_name=NULL,Title=NULL,plot_it=T){
     x_meas_no_sd= x_meas_[!grepl("_sd",x_meas_$variable),]
     ggstics= ggstics+
       geom_point(data= x_meas_no_sd,
-                 aes(y= value, colour= Dominance,pch= Version))+
+                 aes(x=Date, y= value, colour= Dominance,pch= Version))+
       labs(pch='Observations')
     # If there are sd values in the observation file:
     if(any(grepl("_sd",x_meas_$variable))){
@@ -155,7 +172,7 @@ plot_output= function(..., Vars=NULL,obs_name=NULL,Title=NULL,plot_it=T){
 
       }
       ggstics= ggstics+
-        geom_errorbar(data= x_meas_sd,aes(ymin= value_min, ymax= value_max,
+        geom_errorbar(data= x_meas_sd,aes(x=Date, ymin= value_min, ymax= value_max,
                                           colour= Dominance,pch= Version))
     }
 
