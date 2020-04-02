b5e6cc35127d764ce43c38abe32b1dac3e9b5eea
remi.vezy <VEZY@users.noreply.github.com>
2020-04-02 16:13:37 +0200

    Manage preference files using list_stics_exe().

--- R/list_stics_exe.R
+++ R/list_stics_exe.R
@@ -8,6 +8,9 @@
 #' - stics_list: named list of the stics executable
 #' - current: the executable currently in use
 #'
+#' @details The information is read from the `preference.xml` file in JavaStics. The function first
+#' controls that it exists and is complete, and if not it creates it using an OS-specific version.
+#'
 #' @examples
 #' \dontrun{
 #'  list_stics_exe("path/to/JavaSTICS-v131-stics-v841")
@@ -21,25 +24,26 @@ list_stics_exe <- function(javastics_path){
   # checking javastics path
   check_java_path(javastics_path)
 
-  # If the preferences file does not exist, it means JavaStics uses the default one:
-  if(!exists_javastics_pref(javastics_path)){
-    #warning("No model have been selected in JavaStics, add model first !")
-    if(is_windows()){
-      models= list(stics_list= list(modulostics= "stics_modulo.exe"),
-                   current= list(modulostics= "stics_modulo.exe"))
-    }else if(is_unix()){
-      models= list(stics_list= list(modulostics_linux= "stics_modulo"),
-                   current= list(modulostics_linux= "stics_modulo"))
-    }else if(is_mac()){
-      models= list(stics_list= list(modulostics_mac= "stics_modulo_mac"),
-                   current= list(modulostics_mac= "stics_modulo_mac"))
-    }else{
-      stop("Can't set the stics model executable: unable to recognize the operating system")
+  # If the preferences file does not exist, or is incomplete, it means JavaStics was
+  # never used before. So we have to use a template for the preferences.
+  is_pref= exists_javastics_pref(javastics_path)
+
+  config_pref= file.path(javastics_path,"config","preferences.xml")
+
+  if(!is_pref){
+    cli::cli_alert_info("Preference file not found in {.code javastics_path}.")
+    init_javastics_pref(javastics_path, overwrite = FALSE)
+  }else{
+    # If the preferences is availabble, control that it is complete (it is not on at install)
+    xml_pref= SticsRFiles:::xmldocument(config_pref)
+    current_stics= SticsRFiles:::getValues(xml_pref,'//entry[@key="model.last"]')
+    if(is.null(current_stics)){
+      cli::cli_alert_info("Preference file in {.code javastics_path} was found incomplete.")
+      init_javastics_pref(javastics_path, overwrite = TRUE)
     }
-    return(models)
   }
 
-  xml_pref= SticsRFiles:::xmldocument(file.path(javastics_path,"config","preferences.xml"))
+  xml_pref= SticsRFiles:::xmldocument(config_pref)
   current_stics= SticsRFiles:::getValues(xml_pref,'//entry[@key="model.last"]')
 
   stics_list= SticsRFiles:::getValues(xml_pref,'//entry[@key="model.list"]')
@@ -73,3 +77,52 @@ exists_javastics_pref <- function(javastics_path){
   return(file.exists(file.path(javastics_path,"config","preferences.xml")))
 }
 
+
+#' Initialize JavaStics preferences
+#'
+#' @description Initialize the JavaStics `preferences.xml` file.
+#'
+#' @param javastics_path
+#'
+#' @return `TRUE` if the file was created, `FALSE` otherwise.
+#'
+#' @examples
+#' \dontrun{
+#' init_javastics_pref("path/to/javastics")
+#' }
+#'
+#' @keywords internal
+init_javastics_pref= function(javastics_path,overwrite= FALSE){
+
+  check_java_path(javastics_path)
+
+  config_pref= file.path(javastics_path,"config","preferences.xml")
+
+  if(file.exists(config_pref)&&overwrite==FALSE){
+    cli::cli_alert_danger(paste0("Preference file already exists, try with {.code overwrite= TRUE}"))
+    return(FALSE)
+  }
+
+  if(is_windows()){
+    pref_file <- "extdata/preferences/preferences_win.xml"
+  }else if(is_unix()){
+    pref_file <- "extdata/preferences/preferences_lin.xml"
+  }else if(is_mac()){
+    pref_file <- "extdata/preferences/preferences_mac.xml"
+  }else{
+    stop("Unable to recognize the operating system")
+  }
+
+
+  pref_copy= file.copy(from = system.file(pref_file, package = "SticsOnR", mustWork = TRUE),
+                       to = normalizePath(config_pref, mustWork = FALSE), overwrite = overwrite)
+
+  if(pref_copy){
+    cli::cli_alert_success("Preference file added in: {.val {config_pref}}")
+    return(TRUE)
+  }else{
+    cli::cli_alert_danger(paste0("Couldn't add a {.val preference.xml} file in the JavaStics installation. ",
+                                 "Please run {.pkg Javastics} once to create it."))
+    return(FALSE)
+  }
+}
